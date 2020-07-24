683ed699f6740e72f717eb1034dbac29f231709c
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-24 18:41:16 +0300

    installed antd

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
+import 'antd/dist/antd.css';
 import './index.css';
 import App from './components/app';
 import { createStore, applyMiddleware, compose } from 'redux';
