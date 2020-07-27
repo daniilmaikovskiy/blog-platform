d1582727093bf596de9bc80f29aa1ed5f99f8237
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-27 20:36:23 +0300

    added Header

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { wrapper } from './app.module.scss';
+import Header from '../header';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className={wrapper} style={{ minHeight: window.innerHeight }} />
+    <Router>
+      <div className={wrapper} style={{ minHeight: window.innerHeight }}>
+        <Header />
+        
+      </div>
+    </Router>
   );
 };
 
