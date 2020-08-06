4580cb4f73eea7f2c7017f3e67918df86252d0ee
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-08-06 15:08:25 +0300

    refactoring

--- src/selectors.jsx
+++ src/selectors.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Article from './components/article';
+import { NUMBER_OF_ARTICLES_ON_PAGE } from './global-settings';
 
 const selectors = {
   articleArray: ({ articles }) => articles.map((el) => <Article key={el.slug} data={el} />),
-  totalPages: ({ articlesCount }) => articlesCount / 5 + (articlesCount % 5 ? 1 : 0),
+  totalPages: ({ articlesCount }) =>
+    articlesCount / NUMBER_OF_ARTICLES_ON_PAGE +
+    (articlesCount % NUMBER_OF_ARTICLES_ON_PAGE ? 1 : 0),
 };
 
 export default selectors;
